34a5d24b7448e4ed66ad9c3bfb089bf52deb97c5
EmanuelN <emanuelneves10@gmail.com>
2017-08-22 16:04:12 -0400

    getting messages from server and rendering LIVE

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,22 @@ class App extends Component {
     this.socket = new WebSocket("ws://localhost:3001")
     this.state = {
       currentUser: {name: "Emanuel"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          id: '000001',
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: '000002',
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: [] //messages from the server will be stored here as they arrive
     }
   }
   componentDidMount() {
     this.socket.onopen = (e) =>{
       console.log('Connected to server');
     }
+
+    this.socket.onmessage = (event)=>{
+      const eventJSON = JSON.parse(event.data);
+      const oldMessages = this.state.messages
+      const newMessages = oldMessages.concat(eventJSON)
+      this.setState({messages: newMessages})
+    }
+
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
   }
   render() {
     return (
@@ -51,9 +39,7 @@ class App extends Component {
 
   newPost = (content, user) =>{
 
-    const newid = this.state.messages.length * 2
     const newMessage = {
-        id: newid,
         username: "Anonymous",
         content: content
       }
@@ -61,9 +47,6 @@ class App extends Component {
       newMessage.username = user;
     }
     this.socket.send(JSON.stringify(newMessage))
-    const oldMessages = this.state.messages
-    const newMessages = oldMessages.concat(newMessage)
-    this.setState({messages: newMessages})
   }
 }
 export default App;
