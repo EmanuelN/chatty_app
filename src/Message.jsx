66038030a5aebe2a1f8546a344d6f7571f64bfbf
EmanuelN <emanuelneves10@gmail.com>
2017-08-24 13:10:21 -0400

    added image functionality

--- src/Message.jsx
+++ src/Message.jsx
@@ -12,12 +12,26 @@ class Message extends Component {
       const style = {
         color: this.props.message.userColor
       }
-      return (
-        <div className='message'>
-          <span className='message-username' style={style}>{this.props.message.username}</span>
-          <span className='message-content'>{this.props.message.content}</span>
-        </div>
+      const imgRegex = /(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?[.png|.jpg|.gif]/
+      if (imgRegex.test(this.props.message.content)){
+        const imgURL = this.props.message.content.match(imgRegex)[0];
+        const otherContent = this.props.message.content.replace(imgRegex, '');
+        return (
+          <div className='message'>
+            <span className='message-username' style={style}>{this.props.message.username}</span>
+            <span className='message-content'>{otherContent}<br/><img src= {imgURL}/></span>
+
+          </div>
+        );
+      } else {
+        return (
+          <div className='message'>
+            <span className='message-username' style={style}>{this.props.message.username}</span>
+            <span className='message-content'>{this.props.message.content}</span>
+          </div>
       );
+      }
+
     }
   }
 }
